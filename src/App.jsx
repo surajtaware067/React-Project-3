b3ec379d1172eac2f8d6c8c5243be5773247b871
Suraj Taware <surajtaware067@gmail.com>
2024-09-26 17:16:28 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import styled from "styled-components"
 import StartGame from "./componentes/StartGame"
+import { useState } from "react"
+import GamePlay from "./componentes/GamePlay";
 
 
 function App() {
   
+const [isGameStarted,setIsGameStarted]=useState(false);
 
+const toggleGamePlay = ()=> {
+  setIsGameStarted((prev) => !prev);
+}
   return (
     <>
-      <StartGame/>
+      {isGameStarted ? <GamePlay/> : <StartGame toggle={toggleGamePlay}/>}
     </>
   )
 }
--- src/componentes/StartGame.jsx
+++ src/componentes/StartGame.jsx
@@ -1,11 +1,36 @@
+import styled from "styled-components"
+import { Button } from "../styled/Button";
 
-
-const StartGame = () => {
+const StartGame = ({toggle}) => {
   return (
-    <div>
-        Start game
-    </div>
+    <Container>
+       <div>
+        <img src="/Images/dices 1.png" alt="dices image"/>
+        </div>
+        <div className="content">
+            <h1>Dice Game</h1>
+            <Button
+             onClick={toggle}>Play Now</Button>
+        </div>
+    </Container>
   )
 }
 
-export default StartGame
+export default StartGame;
+
+const Container = styled.div`
+  max-width: 1180px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+
+  .content {
+    h1 {
+     font-size: 96px;
+     white-space: nowrap;
+    }
+  }
+`;
+
+
